cf061f261c925c58d6380d9eaa9d7185fd321ff4
tero-yrjola <yrjola.tero@gmail.com>
2018-09-18 13:52:08 +0300

    Add a simple controllable player

---
+++ src/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private Rigidbody2D playerBody;
+    public float playerSpeed = 4f;
+
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 inputVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        playerBody.velocity = inputVelocity * playerSpeed;
+    }
+}
