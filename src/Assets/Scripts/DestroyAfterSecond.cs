05ef71fa42521550bbfb47118ca5c1f71208c5ad
tero-yrjola <yrjola.tero@gmail.com>
2018-09-18 17:45:56 +0300

    Add a jump animation to player

---
+++ src/Assets/Scripts/DestroyAfterSecond.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSecond : MonoBehaviour {
+	void Start () {
+	Destroy (gameObject, 1f);	
+	}
+}
--- src/Assets/Scripts/PlayerMovementScript.cs
+++ src/Assets/Scripts/PlayerMovementScript.cs
@@ -3,17 +3,48 @@ using System.Collections;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    private Rigidbody2D playerBody;
     public float playerSpeed = 4f;
+    public GameObject jumpDustCloud;
+    private Rigidbody2D playerBody;
+    private Transform transform;
+    private Vector3 defaultScale;
+    private bool isAirborne;
 
     void Start()
     {
         playerBody = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+        defaultScale = transform.localScale;
+    }
+
+    void Update(){
+        if (!isAirborne && Input.GetButtonDown("Jump"))
+        {
+            Instantiate(jumpDustCloud, transform.position, jumpDustCloud.transform.rotation);
+            Jump();
+        }
     }
 
     void FixedUpdate()
     {
+        if (isAirborne)
+            transform.localScale = defaultScale * 1.5f;
+        else transform.localScale = defaultScale;
+
         Vector2 inputVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         playerBody.velocity = inputVelocity * playerSpeed;
+    }
+
+    private void Jump()
+    {
+        isAirborne = true;
+        gameObject.layer = 8;
+        Invoke("SetAirborneToFalse", 1);
+    }
+
+    private void SetAirborneToFalse()
+    {
+        gameObject.layer = 1;
+        isAirborne = false;
     }
 }
