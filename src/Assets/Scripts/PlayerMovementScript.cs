bd38aabff3ca7105055fcfba18df629d9f77977a
Joona Juusti <joona.juusti@gmail.com>
2018-09-21 10:23:12 +0300

    Add rotation for player and make camera rotate too

--- src/Assets/Scripts/PlayerMovementScript.cs
+++ src/Assets/Scripts/PlayerMovementScript.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    public float movementSpeed = 4f;
-    public float jumpForce = 100f;
+    public float movementSpeed = 50f;
+    public float jumpForce = 1000f;
+	public float rotationSpeed = 100f;
     public GameObject jumpDustCloud;
     
     private Rigidbody playerBody;
@@ -37,18 +38,26 @@ public class PlayerMovementScript : MonoBehaviour
 
         void Update()
     {	
-		
-        Vector3 inputVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        Vector3 inputVelocity = new Vector3(
+			Input.GetAxisRaw("Vertical") * Mathf.Sin(transform.rotation.eulerAngles.y * Mathf.Deg2Rad) +
+			Input.GetAxisRaw("Horizontal") * Mathf.Sin((transform.rotation.eulerAngles.y + 90) * Mathf.Deg2Rad),
+			0,
+			Input.GetAxisRaw("Vertical") * Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.Deg2Rad) + 
+			Input.GetAxisRaw("Horizontal") * Mathf.Cos((transform.rotation.eulerAngles.y + 90) * Mathf.Deg2Rad)
+		).normalized;
         movementDirection = inputVelocity * movementSpeed;
         if (isGrounded || hasDoubleJumpLeft)
         {
-            if (Input.GetButtonDown("Jump"))
-            {
-                Instantiate(jumpDustCloud, transform.position, jumpDustCloud.transform.rotation);
-                hasDoubleJumpLeft = false;
-                movementDirection.y = jumpForce;
-            }
+			if (Input.GetButtonDown ("Jump")) {
+				Instantiate (jumpDustCloud, transform.position, jumpDustCloud.transform.rotation);
+				hasDoubleJumpLeft = false;
+				movementDirection.y = jumpForce;
+			}
         }
+		if (Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(-Vector3.up * rotationSpeed * Time.deltaTime);
+		if (Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
         playerBody.AddForce(movementDirection); 
     }
 }
